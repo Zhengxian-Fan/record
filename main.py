f5ff539beac97c1285c8d3fea0b18c2e7605a031
faneggy <32335475+faneggy@users.noreply.github.com>
2020-01-14 21:03:42 +0000

    Update main.py

--- main.py
+++ main.py
@@ -203,8 +203,8 @@ with tf.device("/gpu:0"):
   loss = build_model()
     
 
-sess = tf.Session(config=tf.ConfigProto(config=tf.ConfigProto(allow_soft_placement=True, \
-                  log_device_placement=True)))
+sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True, \
+                  log_device_placement=True))
 
 sess.run(tf.global_variables_initializer())
 saver = tf.train.Saver()
